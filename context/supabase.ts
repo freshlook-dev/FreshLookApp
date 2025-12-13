b74c32077183469196e6b29f5f5228911a2bb25b
freshlook-dev <aesthetics.freshlook@gmail.com>
2025-12-13 21:41:31 +0100

    debug supabase env

--- context/supabase.ts
+++ context/supabase.ts
@@ -2,6 +2,9 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+console.log("SUPABASE URL:", process.env.EXPO_PUBLIC_SUPABASE_URL);
+console.log("SUPABASE KEY:", process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
