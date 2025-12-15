2bb536bfbcfe78fbea849ef6786188e39a6e1a1e
freshlook-dev <aesthetics.freshlook@gmail.com>
2025-12-15 01:34:45 +0100

    Update tab bar 3

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -84,7 +84,7 @@ export default function TabsLayout() {
           backgroundColor: '#FFFFFF',
           borderTopWidth: 1.0,
           borderTopColor: '#EEE',
-          height: 100 + insets.bottom,
+          height: 80 + insets.bottom,
           paddingBottom: Math.max(insets.bottom - 6, 8),
         },
 
