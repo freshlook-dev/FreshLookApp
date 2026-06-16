a335865718ef20e15fdf5ebff3254adfa143831e
freshlook-dev <aesthetics.freshlook@gmail.com>
2026-06-16 23:29:24 +0200

    register visit fix

--- app/client/book.tsx
+++ app/client/book.tsx
@@ -26,7 +26,7 @@ type Service = {
   sale_price?: number | null;
 };
 
-const LOCATIONS = ['Prishtine', 'Fushe Kosove'];
+const LOCATIONS = ['Prishtinë', 'Fushë Kosovë'];
 const TIME_SLOTS = ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'];
 const BOOKING_URL = 'https://www.freshlook-ks.com/api/appointments';
 
